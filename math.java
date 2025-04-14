324469b3b58cc2b7412d8371d53c39fee8fbd2e1
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2025-04-14 07:31:09 +0530

    nice

---
+++ math.java
@@ -0,0 +1,15 @@
+class math{
+    public static void main(String a[]){
+        byte x=12;
+        byte y=56;
+        System.out.println("maximum element is :"+Math.max(x,y));
+        System.out.println("minimum element is :"+Math.min(x,y));
+        System.out.println("Sqare Root  :"+Math.sqrt(100));
+        System.out.println("Sqare Root  :"+Math.sqrt(65));
+        System.out.println("Absolute positive value:"+Math.abs(-6.65));
+        
+        int ran=(int)(Math.random()*101);
+        System.out.println("Random integer value beetween 1 to 100 =>"+ran);
+
+    }
+}
